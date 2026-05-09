4b235997af53498ab150e2426ce9dca19b3b8b83
Rishita Sharma <rishitasharma080205@gmail.com>
2026-05-09 15:35:57 +0530

    fix railway module path issue

--- server/server.js
+++ server/server.js
@@ -2,15 +2,15 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const dotenv = require('dotenv')
+const path = require('path')
 
 dotenv.config()
 
 const app = express()
-
 app.use(cors())
 app.use(express.json())
-app.use('/api/auth', require('./routes/authRoutes'))
 app.use('/api/projects', require('./routes/projectRoutes'))
+app.use('/api/auth', require(path.join(__dirname, 'routes/authRoutes')))
 app.use('/api/tasks', require('./routes/taskRoutes'))
 
 mongoose.connect(process.env.MONGO_URI, {
