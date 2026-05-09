62981cf11a63cf2ad370c07d79310e140a1f1ecd
Rishita Sharma <rishitasharma080205@gmail.com>
2026-05-09 16:13:02 +0530

    fix model import casing issue

--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 
-const Task = require('../models/Task')
+const Task = require('../models/task')
 const authMiddleware = require('../middleware/authMiddleware')
 
 const router = express.Router()
