29ea383a9af7346325b5afb7e82ab2bede091617
Rishita Sharma <rishitasharma080205@gmail.com>
2026-05-09 15:41:14 +0530

    fix railway module import issues

--- server/server.js
+++ server/server.js
@@ -10,12 +10,9 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use('/api/projects', require('./routes/projectRoutes'))
-app.use('/api/auth', require(path.join(__dirname, 'routes/authRoutes')))
+app.use('/api/auth', require('./routes/authRoutes'))
 app.use('/api/tasks', require('./routes/taskRoutes'))
-
-mongoose.connect(process.env.MONGO_URI, {
-  tls: true,
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('MongoDB Connected'))
 .catch((err) => console.log(err))
 
